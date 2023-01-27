a80ee40140cd2c2661fc6f1335ee45cb5da2cd57
doczimilan3 <doczi.milan3@gmail.com>
2023-01-27 13:29:48 +0100

    4-re bontás

--- ipcim/src/ipcim/Ipcim.java
+++ ipcim/src/ipcim/Ipcim.java
@@ -50,20 +50,43 @@ public class Ipcim {
         }
         
         int szam = Integer.valueOf(ipfelbont[0]);
+        char type = '0';
         
         if (szam <= 127) {
             System.out.println("Osztály: A");
             System.out.println("Subnet Mask: 255.0.0.0");
+            type = 'A';
         } else if (szam <= 191) {
             System.out.println("Osztály: B");
             System.out.println("Subnet Mask: 255.255.0.0");
+            type = 'B';
         } else if (szam <= 223) {
             System.out.println("Osztály: C");
             System.out.println("Subnet Mask: 255.255.255.0");
+            type = 'C';
         } else if (szam <= 239) {
             System.out.println("Osztály: D");
+            type = 'D';
         } else {
             System.out.println("Osztály: E");
+            type = 'E';
+        }
+        
+        if (type == 'C') {
+            System.out.println();
+            int hanyFele = 4;
+            int lepes = (255/hanyFele)+1;
+            System.out.println("Lépés: " + lepes);
+            String halozat = ipfelbont[0] + "." + ipfelbont[1] + "." + ipfelbont[2] + ".";
+            
+            for (int i = 0; i < hanyFele; i++) {
+                System.out.println((i+1) + ". hálózat");
+                System.out.println("\tHálózati cím: " + halozat + lepes*i);
+                System.out.println("\tElső kiosztható cím: " + halozat + (lepes*i+1));
+                System.out.println("\tUtolsó kiosztható cím: " + halozat + (lepes*(i+1)-2));
+                System.out.println("\tBroadcast cím: " + halozat + (lepes*(i+1)-1));
+                System.out.println();
+            }
         }
     }
     
